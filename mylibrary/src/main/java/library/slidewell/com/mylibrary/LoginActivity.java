1452085ee0efd95bcf415c2904bf6a46661deabe
Kushal <kushal.dixit@acmeitservices.com>
2018-10-24 16:00:35 +0530

    login

--- mylibrary/src/main/java/library/slidewell/com/mylibrary/LoginActivity.java
+++ mylibrary/src/main/java/library/slidewell/com/mylibrary/LoginActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatEditText;
 import android.support.v7.widget.CardView;
 import android.view.View;
 import android.widget.Button;
@@ -25,7 +26,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
-   EditText e1,e2;
+  EditText e1,e2;
    Button btnLogin;
    public static String loginResponse=null;
     public static String LOGINURL=null;
