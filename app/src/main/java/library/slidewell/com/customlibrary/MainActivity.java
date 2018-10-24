c4e4db42ce9516783c8004a852153ca23f72dba0
Kushal <kushal.dixit@acmeitservices.com>
2018-10-24 18:16:12 +0530

    UPDATE

--- app/src/main/java/library/slidewell/com/customlibrary/MainActivity.java
+++ app/src/main/java/library/slidewell/com/customlibrary/MainActivity.java
@@ -12,7 +12,7 @@ import library.slidewell.com.customlibrary.CustomView.Login;
 import library.slidewell.com.customlibrary.CustomView.LoginActivity;
 
 public class MainActivity extends LoginActivity {
-  Login l1;
+  Login l2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
